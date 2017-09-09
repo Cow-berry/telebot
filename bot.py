405c9ab8cf53f37bf9a16a43e58020ec5f2aa2fe
cowberry <tolya.cow@gmail.com>
2017-09-09 23:27:29 +0300

    more files + some new functions

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
 # -*- codimg: utf-8 -*-
 
+import logging
 import config  # в нём лежит ваш токен
 import telebot  # для работы с телеграмом
 import datetime  # для работы со временем
 import time
 from pathlib import Path  # для проверки существования файла
-import os  # для удаления файлов
+from telebot import types # для крутых клавиатур
 
 bot = telebot.TeleBot(config.token)  # создаём объект бота
-
+helpmess = ["/hw =>дз на завтра если оно есть",
+"/hwdate [дата в формате dd.mm.yyyy d-day, m-month, y-year] =>  дз на определённую дату если оно есть",
+"/allhw  =>вся актуальную домашку",
+"/timetable   =>всё известное расписание",
+"/wish [текст пожелания] => автор бота прочтёт её",
+"/info  => важная организационная информация",
+"/help => список всех этих команд"]
 
 def log(message):
     logfile = open("log.txt", 'a')
@@ -30,7 +37,7 @@ def add_file(name, text):
     file.close()
 
 def is_autor(id):
-    if id == config.id:
+    if (id == config.id) or (id == config.kesha_id) or (id == config.kesha_id):
         return True
     else:
         return False
@@ -39,6 +46,8 @@ def is_autor(id):
 def tommorrow_hw(message):  # функция обрабатывающая сообщение. message -- объект, который хранит оочень много информации о пользователе, также хранит текст сообщения
     log(message)
     tommorrowx = datetime.date.today() + datetime.timedelta(days=1)
+    if datetime.date.isoweekday(datetime.date.today()) == 6 :
+        tommorrowx = tommorrowx + datetime.timedelta(days=1)
     tommorrow = tommorrowx.strftime("%d.%m.%Y")
     try:
         hwfile = open(tommorrow + "_hw.txt", 'r')
@@ -97,7 +106,20 @@ def writehw(message):
     add_file(date + "_hw.txt", hwfin)
     bot.send_message(message.chat.id, "Запись д/з произведена успешно")
 
-
+@bot.message_handler(commands=['remove_hw'])
+def rm_hw (message):
+     autor = is_autor(message.chat.id)
+     if not(autor):
+         access = open("access.txt", 'a')
+         access.write(message.chat.first_name + " " + message.chat.last_name + "\nник -- " + str(
+             message.chat.username) + "\nid - " + str(message.chat.id) + "\nтекст - " + message.text + "\n\n")
+         access.close()
+         bot.send_message(message.chat.id, "Отказано в доступе")
+         return
+     date = message.text[11:21]
+     write_file(date+"_hw.txt", '')
+     bot.send_message(message.chat.id, "ДЗ на "+date+" успешно удалено.")
+    
 @bot.message_handler(commands=["rewri"])
 def rewrite(message):
     autor = is_autor(message.chat.id)
@@ -120,9 +142,10 @@ def wish(message):
     id = str(message.chat.id)
     wish = message.text[6:]
     wishes = open("wishes.txt", 'a')
-    wishes.write("имя - " + name + "\n id - " + id + "\nпожелание - " + wish + "\n\n")
+    wishes.write(time.strftime("%d.%m.%Y  %H:%M:%S", time.localtime(time.time())) + "\n" + "имя - " + name + "\n id - " + id + "\nпожелание - " + wish + "\n\n")
     wishes.close()
     bot.send_message(message.chat.id, "Спасибо за пожелание " + message.chat.first_name + ". Автор этого бота скоро его прочитает.")
+    bot.send_message(config.id, "Пришло пожелание от " + name + " : " + wish)
 
 
 @bot.message_handler(commands=["info"])
@@ -174,6 +197,83 @@ def add_info(message):
     add_file("info.txt", infofin)
     bot.send_message(message.chat.id, "Информация записана успешно")
 
+@bot.message_handler(commands=["send"])
+def send (message):
+    autor = is_autor(message.chat.id)
+    if not (autor):
+        access = open("access.txt", 'a')
+        access.write(message.chat.first_name + " " + message.chat.last_name + "\nник -- " + str(
+            message.chat.username) + "\nid - " + str(message.chat.id) + "\nтекст - " + message.text + "\n\n")
+        access.close()
+        bot.send_message(message.chat.id, "Отказано в доступе")
+        return
+    log(message)
+    text = message.text
+    id = text[6:15]
+    mes = text[16:] 
+    try:
+        bot.send_message(id, mes)
+    except Exception:
+        bot.send_message(message.chat.id, "возможно вы ввели несуществующий id")
+@bot.message_handler(commands=["timetable"])
+def timetable (message):
+    log(message)
+    timetablef = open("timetable.txt", 'r')
+    lines = timetablef.readlines()
+    for i in lines:
+        if i == '':
+            bot.send_message(message.chhat.id, '.')
+        else:
+            bot.send_message(message.chat.id, i)
+    timetablef.close()
 
-if __name__ == '__main__':  # создаю цикл бесконечного ожидания сообщений
-    bot.polling()
+@bot.message_handler(commands=["allhw"])
+def all_hw(message):
+    log(message)
+    allhwf = open("allhw.txt", 'r')
+    lines = allhwf.readlines()
+    for i in lines:
+        if i == '':
+            bot.send_message(message.chhat.id, '.')
+        else:
+            bot.send_message(message.chat.id, i)
+    allhwf.close()
+
+@bot.message_handler(commands=["help"])
+def help(message):
+    id = message.chat.id
+    log(message)
+    for i in helpmess:
+        bot.send_message(id, i)
+
+@bot.message_handler(commands=["hvsub"])
+def hwsub(message):
+    sub = message.text[7:]
+    keyboard = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+    try:
+        subfile = open("subjects.txt", 'r')
+    except Exception as e
+        logging.error(e)
+        return
+    subs = subfile.readlines()
+    keyboard.add(*[types.KeyboardButton(name) for name in subs])
+    sent = bot.send_message(message.chat.id, 'По какому предмету?', reply_markup=keyboard)
+    subfile.close()
+    bot.register_next_step_handler(sent, hwsent)
+
+def hwsent(message):
+    try:
+        subfile = open(message.text + ".txt", 'r')
+    except Exception as e
+        logging.error(e)
+        return
+
+
+
+while True:
+    try:
+        bot.polling()
+        time.sleep(1)
+    except Exception as e:
+        logging.error(e)
+        time.sleep(1)
